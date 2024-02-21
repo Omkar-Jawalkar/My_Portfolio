96b77b75698ee55e1ed280f423533a20a58d515c
Omkar-Jawalkar <omkar@goreeva.com>
2024-02-21 21:33:09 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -16,37 +16,42 @@ import "./hover-min.css";
 import "./App.css";
 
 function App() {
-  useEffect(() => {
-    AOS.init({ duration: 2000 });
-  }, []);
+    useEffect(() => {
+        AOS.init({ duration: 2000 });
+    }, []);
 
-  return (
-    <div>
-      <Router>
-        <HeaderSection />
-        <div className="bg-slate-500  mx-4 sm:mx-10 md:mx-25 lg:mx-40">
-          <FirstSection />
-          <span id="experience"></span>
-          <Experience />
-          <span id="projects"></span>
-          <Project />
-          <span id="skills"></span>
-          <SkillSection />
-          <span id="contact"></span>
-          <ContactMe />
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-            <path
-              fill="#101820FF"
-              fill-opacity="1"
-              d="M0,224L120,197.3C240,171,480,117,720,122.7C960,128,1200,192,1320,224L1440,256L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
-            ></path>
-          </svg>
-          <Socials />
+    return (
+        <div>
+            <Router>
+                <HeaderSection />
+
+   
+                <div className="bg-slate-500  mx-4 sm:mx-10 md:mx-25 lg:mx-40">
+                    <FirstSection />
+                    <span id="experience"></span>
+                    <Experience />
+                    <span id="projects"></span>
+                    <Project />
+                    <span id="skills"></span>
+                    <SkillSection />
+                    <span id="contact"></span>
+                    <ContactMe />
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 1440 320"
+                    >
+                        <path
+                            fill="#101820FF"
+                            fill-opacity="1"
+                            d="M0,224L120,197.3C240,171,480,117,720,122.7C960,128,1200,192,1320,224L1440,256L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
+                        ></path>
+                    </svg>
+                    <Socials />
+                </div>
+                <Footer />
+            </Router>
         </div>
-        <Footer />
-      </Router>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/Components/HeaderSection.js
+++ src/Components/HeaderSection.js
@@ -10,148 +10,170 @@ const contentStyle = { borderRadius: "15px", padding: "20px", width: "500px" };
 
 // console.log("This is letsConnect", letsConnect);
 const HeaderSection = () => {
-  const [showMenu, setShowMenu] = React.useState(false);
+    const [showMenu, setShowMenu] = React.useState(false);
 
-  const handleMenu = () => {
-    setShowMenu(!showMenu);
-  };
+    const handleMenu = () => {
+        setShowMenu(!showMenu);
+    };
 
-  return (
-    <>
-      <div
-        style={{ backgroundColor: "#101820FF" }}
-        className={`hidden md:overflow-hidden py-10 px-[50px] lg:px-[155px] z-20 md:flex flex-row justify-between items-center left-0 top-0 right-0 fixed h-16 text-black`}
-      >
-        <div className=" flex ">
-          <a alt="" href="/" className=" ml-2 mr-10">
-            <img alt="" className="w-[70px] h-[70px] " src={logo1} />
-          </a>
-          <ul className=" w-4/12 items-center flex items-center justify-between">
-            <li className="text-white mx-2 transition-opacity hover:text-[#006B38FF] transation duration-duration-1000 ease-in ">
-              {" "}
-              <HashLink smooth to="/#about">
-                {" "}
-                About
-              </HashLink>
-            </li>
-            <li className="text-white mx-2 transition-opacity hover:text-[#006B38FF] transation duration-duration-1000 ease-in ">
-              <HashLink smooth to="/#experience">
-                {" "}
-                Experience
-              </HashLink>
-            </li>
-            <li className="text-white mx-2 transition-opacity hover:text-[#006B38FF] transation duration-duration-1000 ease-in ">
-              <HashLink smooth to="/#projects">
-                {" "}
-                Projects
-              </HashLink>
-            </li>
-            <li className="text-white mx-2 transition-opacity hover:text-[#006B38FF] transation duration-duration-1000 ease-in ">
-              <HashLink smooth to="/#skills">
-                {" "}
-                Skills
-              </HashLink>
-            </li>
-            <li className="text-white mx-2 transition-opacity hover:text-[#006B38FF] transation duration-duration-1000 ease-in ">
-              <HashLink smooth to="/#contact">
-                {" "}
-                Contact
-              </HashLink>
-            </li>
-          </ul>
-        </div>
+    return (
+        <div className="flex justify-center md:items-center flex-col">
+            <h2 className="text-black bg-white  text-center text-md md:text-lg my-4 p-4 border w-full">
+                Portfolio is getting new look, Stay Tuned ⚡️🚀
+                <br />
+                Untill then explore my{" "}
+                <a
+                    className="underline text-blue-500"
+                    href="https://github.com/Omkar-Jawalkar"
+                    _target="blank"
+                >
+                    Github
+                </a>
+            </h2>
+            <div
+                style={{ backgroundColor: "#101820FF" }}
+                className={`hidden w-full  md:overflow-hidden py-10 px-[50px] lg:px-[155px] z-20 md:flex flex-row justify-between items-center left-0 top-0 right-0 fixed h-16 text-black`}
+            >
+                <div className="  ">
+                    <a alt="" href="/" className=" ml-2 mr-10">
+                        <img
+                            alt=""
+                            className="w-[70px] h-[70px] "
+                            src={logo1}
+                        />
+                    </a>
+                    <ul className=" w-4/12 items-center flex items-center justify-between">
+                        <li className="text-white mx-2 transition-opacity hover:text-[#006B38FF] transation duration-duration-1000 ease-in ">
+                            {" "}
+                            <HashLink smooth to="/#about">
+                                {" "}
+                                About
+                            </HashLink>
+                        </li>
+                        <li className="text-white mx-2 transition-opacity hover:text-[#006B38FF] transation duration-duration-1000 ease-in ">
+                            <HashLink smooth to="/#experience">
+                                {" "}
+                                Experience
+                            </HashLink>
+                        </li>
+                        <li className="text-white mx-2 transition-opacity hover:text-[#006B38FF] transation duration-duration-1000 ease-in ">
+                            <HashLink smooth to="/#projects">
+                                {" "}
+                                Projects
+                            </HashLink>
+                        </li>
+                        <li className="text-white mx-2 transition-opacity hover:text-[#006B38FF] transation duration-duration-1000 ease-in ">
+                            <HashLink smooth to="/#skills">
+                                {" "}
+                                Skills
+                            </HashLink>
+                        </li>
+                        <li className="text-white mx-2 transition-opacity hover:text-[#006B38FF] transation duration-duration-1000 ease-in ">
+                            <HashLink smooth to="/#contact">
+                                {" "}
+                                Contact
+                            </HashLink>
+                        </li>
+                    </ul>
+                </div>
 
-        <div className="w-2/12">
-          {" "}
-          <Popup
-            trigger={
-              <button className=" px-12 my-3 py-3 text-sm font-medium text-white bg-[#006B38FF] border border-[#006B38FF] rounded active:text-[#006B38FF] hover:bg-transparent hover:text-[#006B38FF] transation duration-300 active:opacity-10 ">
-                Lets Connect
-              </button>
-            }
-            modal
-            {...{ contentStyle }}
-          >
-            <div className=" flex justify-center items-center flex-col">
-              <div className="text-xl border-b-2 pt-1 mb-2">
-                Lets Connect 🤝{" "}
-              </div>
-              <div className="flex justify-center items-center flex-col">
-                {letsConnect.map((connect, index) => (
-                  <div key={index}>
-                    {console.log("This is Connect obj", connect)}
-                    <PopupButton
-                      emoji={connect.emoji}
-                      name={connect.name}
-                      link={connect.link}
-                    />
-                  </div>
-                ))}
-              </div>
+                <div className="w-2/12">
+                    {" "}
+                    <Popup
+                        trigger={
+                            <button className=" px-12 my-3 py-3 text-sm font-medium text-white bg-[#006B38FF] border border-[#006B38FF] rounded active:text-[#006B38FF] hover:bg-transparent hover:text-[#006B38FF] transation duration-300 active:opacity-10 ">
+                                Lets Connect
+                            </button>
+                        }
+                        modal
+                        {...{ contentStyle }}
+                    >
+                        <div className=" flex justify-center items-center flex-col">
+                            <div className="text-xl border-b-2 pt-1 mb-2">
+                                Lets Connect 🤝{" "}
+                            </div>
+                            <div className="flex justify-center items-center flex-col">
+                                {letsConnect.map((connect, index) => (
+                                    <div key={index}>
+                                        {console.log(
+                                            "This is Connect obj",
+                                            connect
+                                        )}
+                                        <PopupButton
+                                            emoji={connect.emoji}
+                                            name={connect.name}
+                                            link={connect.link}
+                                        />
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </Popup>
+                </div>
             </div>
-          </Popup>
-        </div>
-      </div>
 
-      <div className=" md:hidden flex mx-6 justify-between items-center flex-row">
-        <div className=" absolute top-0 sm:left-9 ">
-          <img className="w-[70px] h-[70px] " alt="" src={logo1} />
-        </div>
+            <div className=" md:hidden flex mx-6 justify-between items-center flex-row">
+                <div className=" absolute top-0 sm:left-9 ">
+                    <img className="w-[70px] h-[70px] " alt="" src={logo1} />
+                </div>
 
-        <div>
-          <div className=" absolute right-4 top-5 sm:right-10 ">
-            {!showMenu ? (
-              <AiOutlineMenu
-                onClick={handleMenu}
-                className="text-white text-3xl"
-              />
+                <div>
+                    <div className=" absolute right-4 top-5 sm:right-10 ">
+                        {!showMenu ? (
+                            <AiOutlineMenu
+                                onClick={handleMenu}
+                                className="text-white text-3xl"
+                            />
+                        ) : (
+                            <ImCross
+                                onClick={handleMenu}
+                                className="text-white text-3xl"
+                            />
+                        )}
+                    </div>
+                </div>
+            </div>
+            {showMenu ? (
+                <div className="md:hidden">
+                    <div className="shrink-0 w-full items-center flex-col flex items-center justify-between">
+                        <div className="text-white  border-b-2 border-red-100  my-1 mx-2 transition-opacity hover:text-[#006B38FF] transation duration-duration-1000 ease-in ">
+                            {" "}
+                            <HashLink smooth to="/#about">
+                                {" "}
+                                About
+                            </HashLink>
+                        </div>
+                        <div className="text-white my-1  border-b-2 border-red-100 mx-2 transition-opacity hover:text-[#006B38FF] transation duration-duration-1000 ease-in ">
+                            <HashLink smooth to="/#experience">
+                                {" "}
+                                Experience
+                            </HashLink>
+                        </div>
+                        <div className="text-white border-b-2 border-red-100 my-1 mx-2 transition-opacity hover:text-[#006B38FF] transation duration-duration-1000 ease-in ">
+                            <HashLink smooth to="/#projects">
+                                {" "}
+                                Projects
+                            </HashLink>
+                        </div>
+                        <div className="text-white  border-b-2 border-red-100  my-1 mx-2 transition-opacity hover:text-[#006B38FF] transation duration-duration-1000 ease-in ">
+                            <HashLink smooth to="/#skills">
+                                {" "}
+                                Skills
+                            </HashLink>
+                        </div>
+                        <div className="text-white  border-b-2 border-red-100 my-1 mx-2 mb-3 transition-opacity hover:text-[#006B38FF] transation duration-duration-1000 ease-in ">
+                            <HashLink smooth to="/#contact">
+                                {" "}
+                                Contact
+                            </HashLink>
+                        </div>
+                    </div>
+                </div>
             ) : (
-              <ImCross onClick={handleMenu} className="text-white text-3xl" />
+                <div> </div>
             )}
-          </div>
         </div>
-      </div>
-      {showMenu ? (
-        <div className="md:hidden">
-          <div className="shrink-0 w-full items-center flex-col flex items-center justify-between">
-            <div className="text-white  border-b-2 border-red-100  my-1 mx-2 transition-opacity hover:text-[#006B38FF] transation duration-duration-1000 ease-in ">
-              {" "}
-              <HashLink smooth to="/#about">
-                {" "}
-                About
-              </HashLink>
-            </div>
-            <div className="text-white my-1  border-b-2 border-red-100 mx-2 transition-opacity hover:text-[#006B38FF] transation duration-duration-1000 ease-in ">
-              <HashLink smooth to="/#experience">
-                {" "}
-                Experience
-              </HashLink>
-            </div>
-            <div className="text-white border-b-2 border-red-100 my-1 mx-2 transition-opacity hover:text-[#006B38FF] transation duration-duration-1000 ease-in ">
-              <HashLink smooth to="/#projects">
-                {" "}
-                Projects
-              </HashLink>
-            </div>
-            <div className="text-white  border-b-2 border-red-100  my-1 mx-2 transition-opacity hover:text-[#006B38FF] transation duration-duration-1000 ease-in ">
-              <HashLink smooth to="/#skills">
-                {" "}
-                Skills
-              </HashLink>
-            </div>
-            <div className="text-white  border-b-2 border-red-100 my-1 mx-2 mb-3 transition-opacity hover:text-[#006B38FF] transation duration-duration-1000 ease-in ">
-              <HashLink smooth to="/#contact">
-                {" "}
-                Contact
-              </HashLink>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <div> </div>
-      )}
-    </>
-  );
+    );
 };
 
 export default HeaderSection;
